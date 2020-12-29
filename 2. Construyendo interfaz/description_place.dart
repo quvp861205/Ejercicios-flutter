155d1de738a99c9d3bcb3eebb734ae0289028a19
unknown <quvp861205@gmail.com>
2020-12-29 11:50:02 -0700

    Se pone todo en una columna y variables, ademas de agregar 3 tipos de estrellas

--- 2. Construyendo interfaz/description_place.dart
+++ 2. Construyendo interfaz/description_place.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
 
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
+  String descriptionDummy = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \n'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.";
   @override
   //Esta funcion deberia devolver toda la interfaz de usuario
   Widget build(BuildContext context) {
@@ -17,6 +24,44 @@ class DescriptionPlace extends StatelessWidget {
       )
     );
 
+    final star_half = Container (
+        margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0,
+        ),
+        child: Icon(
+            Icons.star_half,
+            color: Color(0xFFf2C611)
+        )
+    );
+
+    final star_border = Container (
+        margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0,
+        ),
+        child: Icon(
+            Icons.star_border,
+            color: Color(0xFFf2C611)
+        )
+    );
+
+    final description = Container(
+      margin: new EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0
+      ),
+      child: new Text(
+        descriptionPlace,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
+      )
+      )
+    );
+
     final title_stars = Row(
       children: <Widget>[
         Container(
@@ -26,7 +71,7 @@ class DescriptionPlace extends StatelessWidget {
             right: 20.0,
           ),
           child: Text(
-            "Duwili Ella",
+            namePlace,
              style: TextStyle(
                fontSize: 30.0,
                fontWeight: FontWeight.w900
@@ -41,13 +86,18 @@ class DescriptionPlace extends StatelessWidget {
             star,
             star,
             star,
-            star
+            star_half
           ],
         )
       ]
     );
 
-    return title_stars;
+    return Column(
+      children: <Widget>[
+        title_stars,
+        description
+      ],
+    );
   }
 
 }
