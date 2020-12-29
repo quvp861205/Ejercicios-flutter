69e8d177b264f0bff2e49e66ad5f03b5c6fc3575
unknown <quvp861205@gmail.com>
2020-12-29 11:20:30 -0700

    Se agrega un row con un texto y 5 estrellitas en la clase description_place

---
+++ 2. Construyendo interfaz/description_place.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DescriptionPlace extends StatelessWidget {
+
+  @override
+  //Esta funcion deberia devolver toda la interfaz de usuario
+  Widget build(BuildContext context) {
+    final star = Container (
+        margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0,
+        ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2C611)
+      )
+    );
+
+    final title_stars = Row(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(
+            top: 320.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Text(
+            "Duwili Ella",
+             style: TextStyle(
+               fontSize: 30.0,
+               fontWeight: FontWeight.w900
+          ),
+            textAlign: TextAlign.left
+
+          )
+        ),
+        Row(
+          children: <Widget>[
+            star,
+            star,
+            star,
+            star,
+            star
+          ],
+        )
+      ]
+    );
+
+    return title_stars;
+  }
+
+}
